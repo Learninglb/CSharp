d2b82ad2a8118ca32f6b5cca7a99d24bc8599eda
lorib <learninglb@yahoo.com>
2020-10-20 13:27:43 -0600

    Completed 2.Dive into controls and event handlers

--- ExperimentWithControls/ExperimentWithControls/MainWindow.xaml.cs
+++ ExperimentWithControls/ExperimentWithControls/MainWindow.xaml.cs
@@ -34,7 +34,41 @@ namespace ExperimentWithControls
         {
             e.Handled = !int.TryParse(e.Text, out int result);
         }
+
+        private void smallSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = smallSlider.Value.ToString("0");
+        }
+        private void bigSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = bigSlider.Value.ToString("000-000-0000");
+        }
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+                number.Text = radioButton.Content.ToString();
+        }
+        private void myListBox_SelectionChanged(
+            object sender, SelectionChangedEventArgs e)
+        {
+            if (myListBox.SelectedItem is ListBoxItem listBoxItem)
+                number.Text = listBoxItem.Content.ToString();
+        }
+
+        private void ReadOnlyComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (readOnlyComboBox.SelectedItem is ListBoxItem listBoxItem)
+                number.Text = listBoxItem.Content.ToString();
+        }
+
+        private void EditableComboBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out int result);
+            if (sender is ComboBox comboBox)
+                number.Text = comboBox.Text;
+        }
     }
+    
                 
 
 }
