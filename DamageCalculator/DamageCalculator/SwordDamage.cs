09b484856496bc9e99fc5b4e560359c315ff21d1
lorib <learninglb@yahoo.com>
2020-11-05 11:50:49 -0700

    Updates

---
+++ DamageCalculator/DamageCalculator/SwordDamage.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+ namespace DamageCalculator
+ {
+    public class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll;
+        public decimal MagicMultiplier = 1M;
+        public int FlamingDamage = 0;
+        public int Damage;
+
+        public void CalculateDamage()
+        {
+            Damage = (int) (Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+            Debug.WriteLine($"CalculateDamage finished: {Damage} (roll: {Roll}");
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            MagicMultiplier = isMagic ? 1.75M : 1M;
+            CalculateDamage();
+            Debug.WriteLine($"SetMagic finished: {Damage} (roll: {Roll})");
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+            Debug.WriteLine($"SetFlaming finished: {Damage} (roll: {Roll})");
+        }
+    }
+}
---
+++ SwordDamage/SwordDamage/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+
+namespace SwordDamage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwordDamage swordDamage = new SwordDamage();
+            Random random = new Random();
+
+            while (true)
+            {
+                Console.WriteLine("Enter 0 for no magic/flaming, 1 for magic, 2 for flaming, " +
+                                  "3 for both, and anything else to quit.");
+                char key = Console.ReadKey().KeyChar;
+                if (key != '0' && key != '1' && key != '2' && key != '3') return;
+                int roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+
+                swordDamage.Roll = roll;
+                swordDamage.SetMagic(key == '1' || key == '3');
+                swordDamage.SetFlaming(key == '2' || key == '3');
+                Console.WriteLine("\nRolled " + roll + " for " + swordDamage.Damage + " HP\n");
+            } 
+            
+        }
+    }
+}
